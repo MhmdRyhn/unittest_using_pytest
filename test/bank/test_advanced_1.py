dee4ad484d585e331eb9ce6cd213e30d2520a52b
Mahmood Al Rayhan <rayhan.cse12@gmail.com>
2020-02-05 00:45:55 +0600

    advanced test case using pytest fixture

--- test/bank/test_advanced_1.py
+++ test/bank/test_advanced_1.py
@@ -1,8 +1,48 @@
-# This script uses pytest Fixtures
+# -------- This script uses pytest Fixtures -------------
+# In test_simple.py script, we need to create Account 
+# object in each test function. To avoid this, pytest
+# fixture is here to help us.
 
 import pytest
 
 from bank.account import Account, InsufficientBalance
 
 
+@pytest.fixture
+def empty_account():
+    """
+    Returns an Account instance with a zero balance
+    """
+    return Account()
 
+
+@pytest.fixture
+def account():
+    """
+    Returns an Account instance with a balance of 10
+    """
+    return Account(10)
+
+
+def test_default_initial_amount(empty_account):
+    assert empty_account.balance == 0
+
+
+def test_setting_the_initial_amount(account):
+    assert account.balance == 10
+
+
+def test_account_add_cash(account):
+    account.add_cash(5)
+    assert account.balance == 15
+
+
+def test_account_withdraw_cash(account):
+    account.withdraw_cash(5)
+    assert account.balance == 5
+
+
+def test_withdraw_insufficient_amount(empty_account):
+    # When a function raises some Exception, use this way
+    with pytest.raises(InsufficientBalance):
+        empty_account.withdraw_cash(5)
