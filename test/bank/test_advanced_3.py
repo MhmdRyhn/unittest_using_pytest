fc014f98eae97fdad4583b04c4ab9227c19abd24
Mahmood Al Rayhan <rayhan.cse12@gmail.com>
2020-02-05 01:27:01 +0600

    test case using pytest fixture and parametrize test function

---
+++ test/bank/test_advanced_3.py
@@ -0,0 +1,28 @@
+# This script uses Parametrize Test Functions
+# along with pytest fixture
+
+import pytest
+
+from bank.account import Account, InsufficientBalance
+
+
+@pytest.fixture
+def my_account():
+    """
+    Returns an Account instance with a balance of 20
+    """
+    return Account(20)
+
+
+@pytest.mark.parametrize(
+    "added,withdrawn,remaining", 
+    [
+        (10, 25, 5), 
+        (3, 10, 13), 
+        (8, 11, 17)
+    ]
+)
+def test_transactions(my_account, added, withdrawn, remaining):
+    my_account.add_cash(added)
+    my_account.withdraw_cash(withdrawn)
+    assert my_account.balance == remaining
