3c195677b934fdb7ace9a5b8836a22c0ebfcc3f4
Mahmood Al Rayhan <rayhan.cse12@gmail.com>
2020-02-05 00:41:03 +0600

    added more test cases in simple.py script

--- test/bank/test_simple.py
+++ test/bank/test_simple.py
@@ -8,6 +8,23 @@ def test_default_initial_amount():
     assert account.balance == 0
 
 
+def test_setting_the_initial_amount():
+    account = Account(10)
+    assert account.balance == 10
+
+
+def test_account_add_cash():
+    account = Account(10)
+    account.add_cash(5)
+    assert account.balance == 15
+
+
+def test_account_withdraw_cash():
+    account = Account(10)
+    account.withdraw_cash(5)
+    assert account.balance == 5
+
+
 def test_withdraw_insufficient_amount():
     account = Account()
     # When a function raises some Exception, use this way
